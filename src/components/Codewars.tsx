9e18c1442e02afa040e7ad13da735e043dc6fc97
Eric Hoover <ehoover3@yahoo.com>
2023-02-18 20:50:02 -0500

    updated icebreaker question

--- src/components/Codewars.tsx
+++ src/components/Codewars.tsx
@@ -4,3 +4,5 @@ export function add(a: number, b: number): number {
 }
 
 // 2.  Write a multiply function that passes the tests in the Codewars.test.tsx file
+
+// 3.  Ask other people in your group:  What is one of the best pieces advice you have ever been given?
